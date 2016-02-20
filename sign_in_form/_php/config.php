92715f91525866c9940b58d8b907488955552a96
Aung Naing Oo <aung0086@e.ntu.edu.sg>
2016-02-21 01:36:56 +0800

    Deleted some old files

--- sign_in_form/_php/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * DB configuration variables
- */
-  define("DB_HOST", "localhost");
-  define("DB_USER", "naing");
-  define("DB_PASSWORD", "test");
-  define("DB_DATABASE", "prayer_record");
-?>
--- sign_in_form/_php/dB_interface.php
+++
@@ -1,9 +0,0 @@
-<?php
-// The interface will have the CRDUAL operation definition
-    interface Dbinterface{
-      /*This interface will contain all the CRUDL functons*/
-        public function createTable();
-        public function insertData($first, $last, $id, $email, $pass, $dob, $gender);
-        public function checkExist($id_or_email, $pass, $what_is_it); 
-    }
-?>
